c8e0f53b6742ffc43cc203fd971cf56d2a037698
unknown <av3001560@gmail.com>
2022-10-19 00:26:29 +0530

    added basic request form without styling

--- hcms/src/App.js
+++ hcms/src/App.js
@@ -1,10 +1,12 @@
-import logo from './logo.svg';
+import Request from './component/Request';
 import './App.css';
 
 function App() {
   return (
     <div>
-      
+
+		<Request /> 
+		  
     </div>
   );
 }
