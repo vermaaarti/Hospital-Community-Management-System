04f5c247e4d45622700b719710f0a11a5ab6c736
kmark15 <kunal15markam@gmail.com>
2022-12-07 12:57:22 +0530

    lorem ipsum

--- hcms/src/Pages/MainPage.js
+++ hcms/src/Pages/MainPage.js
@@ -16,6 +16,7 @@ const MainPage = () => {
       <section className="bg-img">
         <div className="bg-content">
           <h2>Welcome To HCMS</h2>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
           <button className="read-more">Read more</button>
         </div>
       </section>
