f15dd69be12fbeab7ffa4fbf5f3048b2e3bb01ad
kmark15 <kunal15markam@gmail.com>
2022-12-07 13:17:37 +0530

    Updated cards

--- hcms/src/Pages/MainPage.js
+++ hcms/src/Pages/MainPage.js
@@ -7,8 +7,8 @@ import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 import cardio from "../Assets/Cardiogram.png";
-import service from "../Assets/service.png";
-
+import service from "../Assets/ser.png";
+import register from "../Assets/register.png";
 
 const MainPage = () => {
   return (
@@ -16,7 +16,22 @@ const MainPage = () => {
       <section className="bg-img">
         <div className="bg-content">
           <h2>Welcome To HCMS</h2>
-          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+            aliquip ex ea commodo consequat. Duis aute irure dolor in
+            reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+            pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+            culpa qui officia deserunt mollit anim id est laborum.Lorem ipsum
+            dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
+            incididunt ut labore et dolore magna aliqua. Ut enim ad minim
+            veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex
+            ea commodo consequat. Duis aute irure dolor in reprehenderit in
+            voluptate velit esse cillum dolore eu fugiat nulla pariatur.
+            Excepteur sint occaecat cupidatat non proident, sunt in culpa qui
+            officia deserunt mollit anim id est laborum.
+          </p>
           <button className="read-more">Read more</button>
         </div>
       </section>
@@ -25,11 +40,12 @@ const MainPage = () => {
           <Row>
             <Col>
               <Card style={{ width: "26rem", height: "18rem" }}>
-               <img class= "crd-img" src ={cardio} alt="card-img"/>
+                <img class="crd-img" src={cardio} alt="card-img" />
                 <Card.Body>
                   <Card.Title>Hospitality</Card.Title>
                   <Card.Text>
-                  Clinical excellence must be the priority for any health care service provider.
+                    Clinical excellence must be the priority for any health care
+                    service provider.
                   </Card.Text>
                   <Button variant="primary">Apply for Resources</Button>
                 </Card.Body>
@@ -37,27 +53,29 @@ const MainPage = () => {
             </Col>
             <Col>
               <Card style={{ width: "26rem", height: "18rem" }}>
+                <img src={register} class="crd-img"/>
                 <Card.Body>
-                  <Card.Title>Card Title</Card.Title>
+                  <Card.Title></Card.Title>
                   <Card.Text>
-                    Some quick example text to build on the card title and make
-                    up the bulk of the card's content.
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
+                    do eiusmod tempor incididunt ut labore et dolore magna
+                    aliqua. Ut enim ad.
                   </Card.Text>
-                  <Button variant="primary">Go somewhere</Button>
+                  <Button variant="primary">Register Here</Button>
                 </Card.Body>
               </Card>
             </Col>
             <Col>
               <Card style={{ width: "26rem", height: "18rem" }}>
-                <img src={service} class="crd-img" alt
-                ="card-img"style={{width:"9rem",height:
-                "7rem" }}/>
+                <img src={service} class="crd-img" alt="card-image" />
                 <Card.Body>
                   <Card.Title>Our Services</Card.Title>
                   <Card.Text>
-                    
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
+                    do eiusmod tempor incididunt ut labore et dolore magna
+                    aliqua. Ut enim ad.
                   </Card.Text>
-                  <Button variant="primary">Go somewhere</Button>
+                  <Button variant="primary">Our Services</Button>
                 </Card.Body>
               </Card>
             </Col>
