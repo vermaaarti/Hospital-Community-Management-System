7c5eae8205153fc78b58cb586cfc9c320417d50a
kmark15 <kunal15markam@gmail.com>
2022-12-07 12:50:41 +0530

    upadted the UI

--- hcms/src/Pages/MainPage.js
+++ hcms/src/Pages/MainPage.js
@@ -6,7 +6,9 @@ import Button from "react-bootstrap/Button";
 import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
-import { FaFacebook, FaAmazon } from "react-icons/fa";
+import cardio from "../Assets/Cardiogram.png";
+import service from "../Assets/service.png";
+
 
 const MainPage = () => {
   return (
@@ -22,7 +24,18 @@ const MainPage = () => {
           <Row>
             <Col>
               <Card style={{ width: "26rem", height: "18rem" }}>
-                <FaFacebook />
+               <img class= "crd-img" src ={cardio} alt="card-img"/>
+                <Card.Body>
+                  <Card.Title>Hospitality</Card.Title>
+                  <Card.Text>
+                  Clinical excellence must be the priority for any health care service provider.
+                  </Card.Text>
+                  <Button variant="primary">Apply for Resources</Button>
+                </Card.Body>
+              </Card>
+            </Col>
+            <Col>
+              <Card style={{ width: "26rem", height: "18rem" }}>
                 <Card.Body>
                   <Card.Title>Card Title</Card.Title>
                   <Card.Text>
@@ -35,25 +48,13 @@ const MainPage = () => {
             </Col>
             <Col>
               <Card style={{ width: "26rem", height: "18rem" }}>
-                <FaAmazon />
+                <img src={service} class="crd-img" alt
+                ="card-img"style={{width:"9rem",height:
+                "7rem" }}/>
                 <Card.Body>
-                  <Card.Title>Card Title</Card.Title>
+                  <Card.Title>Our Services</Card.Title>
                   <Card.Text>
-                    Some quick example text to build on the card title and make
-                    up the bulk of the card's content.
-                  </Card.Text>
-                  <Button variant="primary">Go somewhere</Button>
-                </Card.Body>
-              </Card>
-            </Col>
-            <Col>
-              <Card style={{ width: "26rem", height: "18rem" }}>
-                <FaAmazon />
-                <Card.Body>
-                  <Card.Title>Card Title</Card.Title>
-                  <Card.Text>
-                    Some quick example text to build on the card title and make
-                    up the bulk of the card's content.
+                    
                   </Card.Text>
                   <Button variant="primary">Go somewhere</Button>
                 </Card.Body>
